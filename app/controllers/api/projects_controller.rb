d831edb553630a24be0c3b30a042c8d547747250
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-03-30 18:13:07 +0200

    initial

---
+++ app/controllers/api/projects_controller.rb
@@ -0,0 +1,41 @@
+class Api::ProjectsController < ApplicationController
+  before_action :authenticate_user! 
+  
+  def index
+    render json: current_user.projects.map { |project| ProjectSerializer.new(project, root: false) }.to_json
+  end
+
+  def create
+    new_project = Project.new new_project_params
+
+    if new_project.save
+      render json: ProjectSerializer.new(new_project, root: false)
+    else
+      render json: { errors: new_project.errors }
+    end
+  end
+
+  def update
+    if project.update_attributes(update_project_params)
+      render json: ProjectSerializer.new(project, root: false)
+    else
+      render json: { errors: project.errors }
+    end
+  end
+
+  private
+
+  def project
+    @project = Project.find(params[:id]) or raise Exception.new("Project not found")
+  end
+
+  def new_project_params
+    params.require(:project).permit(
+      :name
+    )
+  end
+
+  def update_note_params
+    params.require(:project).permit(:name)
+  end
+end
