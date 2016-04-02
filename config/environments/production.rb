9377f36dc06b410af78f9aace03adb88a3798e5d
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-04-02 21:12:34 +0200

    heroku smtp config:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'smtp.sendgrid.net',
+  config.action_mailer.smtp_settings = { :address => 'https://shop-lab-frontend.herokuapp.com',
                                         :port => 587,
                                         :authentication => :plain,
                                         :user_name      => ENV['SENDGRID_USERNAME'],
