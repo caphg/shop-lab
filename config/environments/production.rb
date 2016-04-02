832afd2013fc2bd7b693f010df4a36f67b8ec147
Hrvoje G <hrvoje.grdic1@gmail.com>
2016-04-02 21:15:45 +0200

    heroku smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,9 +77,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
+  config.action_mailer.default_url_options = { :host => 'https://shop-lab-frontend.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'https://shop-lab-frontend.herokuapp.com',
+  config.action_mailer.smtp_settings = { :address => 'smtp.sendgrid.net',
                                         :port => 587,
                                         :authentication => :plain,
                                         :user_name      => ENV['SENDGRID_USERNAME'],
